24031955009cb214eebb4ca58536745dca3b768a
Max Weffers <rcstar6696@gmail.com>
2019-10-21 18:43:29 +0200

    sdm660: Add twolip support

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter jasmine_sprout wayne clover,$(TARGET_DEVICE)),)
+ifneq ($(filter twolip jasmine_sprout wayne clover,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := chargeonlymode
