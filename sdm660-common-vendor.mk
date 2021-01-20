8298bd9ac01bbfcc0dea0250394e57231b4cf46a
OdSazib <odsazib@gmail.com>
2021-01-21 05:14:45 +0600

    sdm660-common: Update thermal/widevine blobs from jasmine V11.0.19.0.QDIMIXM  * jasmine-user-10-QKQ1.190910.002-V11.0.19.0.QDIMIXM-release-keys
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- sdm660-common-vendor.mk
+++ sdm660-common-vendor.mk
@@ -711,6 +711,7 @@ PRODUCT_PACKAGES += \
     libsdm-disp-vndapis \
     libthermalclient \
     libtime_genoff \
+    manifest_android.hardware.drm@1.2-service.widevine \
     PowerOffAlarm \
     qcrilhook \
     qcrilmsgtunnel \
